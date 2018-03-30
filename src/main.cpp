26fb0a786fb8a1856c6439b2ef6e61084fb755c3
Michael Gonzalez <michael.gonzalez1812@gmail.com>
2018-03-30 13:25:10 -0600

    producto interno vectores

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,30 @@
 
 #include <cstdlib>
 #include <iostream>
-
+#include <iterator>
 #include "LUDoolittle.hpp"
+#include "Matrix.hpp"
 
+//using namespace anpi;
 int main() {
 
-  // Some example code
-  anpi::Matrix<float> A = { {-1,-2,1,2},
-                            { 2, 0,1,2},
-                            {-1,-1,0,1},
-                            { 1, 1,1,1} };
-  anpi::Matrix<float> LU;
 
-  std::vector<size_t> p;
-  anpi::luDoolittle(A,LU,p);
+    // Some example code
+    anpi::Matrix<float> A = { {-1,-2,1,2},
+                              { 2, 0,1,2},
+                              {-1,-1,0,1},
+                              { 1, 1,1,1} };
+    anpi::Matrix<float> LU;
 
-  return 0;
+    std::vector<size_t> p;
+    anpi::luDoolittle(A,LU,p);
+
+    return 0;
 }
-  
+
+//************************************************************
+//Pruebas (NO BORRARLAS)
+//*************************************************************
+
+//std::vector<float> b = {1, 2, 3};
+//std::cout<< b*b;
