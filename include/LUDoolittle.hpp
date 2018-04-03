56a559305998b30942da34a2a383b701b6245531
estape11 <estape11@gmail.com>
2018-04-03 17:28:06 -0600

    Test

--- include/LUDoolittle.hpp
+++ include/LUDoolittle.hpp
@@ -22,49 +22,48 @@
 namespace anpi {
 
 
-  /**
-   * Auxiliary method used to debug LU decomposition.
-   *
-   * It separates a packed LU matrix into the lower triangular matrix
-   * L and the upper triangular matrix U, such that the diagonal of L
-   * is composed by 1's.
-   */
-  template<typename T>
-  void unpackDoolittle(const Matrix<T>& LU,
-                       Matrix<T>& L,
-                       Matrix<T>& U) {
+    /**
+     * Auxiliary method used to debug LU decomposition.
+     *
+     * It separates a packed LU matrix into the lower triangular matrix
+     * L and the upper triangular matrix U, such that the diagonal of L
+     * is composed by 1's.
+     */
+    template<typename T>
+    void unpackDoolittle(const Matrix<T> &LU,
+                         Matrix<T> &L,
+                         Matrix<T> &U) {
 
-    throw anpi::Exception("To be implemented yet");
-  }
-  
-  /**
-   * Decompose the matrix A into a lower triangular matrix L and an
-   * upper triangular matrix U.  The matrices L and U are packed into
-   * a single matrix LU. 
-   *
-   * The L matrix will have in the Doolittle's LU decomposition a
-   * diagonal of 1's
-   *
-   * @param[in] A a square matrix 
-   * @param[out] LU matrix encoding the L and U matrices
-   * @param[out] permut permutation vector, holding the indices of the
-   *             original matrix falling into the corresponding element.
-   *             For example if permut[5]==3 holds, then the fifth row
-   *             of the LU decomposition in fact is dealing with the third
-   *             row of the original matrix.
-   *
-   * @throws anpi::Exception if matrix cannot be decomposed, or input
-   *         matrix is not square.
-   */
-  template<typename T>
-  void luDoolittle(const Matrix<T>& A,
-                   Matrix<T>& LU,
-                   std::vector<size_t>& permut) {
+        throw anpi::Exception("To be implemented yet");
+    }
 
-    throw anpi::Exception("To be implemented yet");
-  }
+    /**
+     * Decompose the matrix A into a lower triangular matrix L and an
+     * upper triangular matrix U.  The matrices L and U are packed into
+     * a single matrix LU.
+     *
+     * The L matrix will have in the Doolittle's LU decomposition a
+     * diagonal of 1's
+     *
+     * @param[in] A a square matrix
+     * @param[out] LU matrix encoding the L and U matrices
+     * @param[out] permut permutation vector, holding the indices of the
+     *             original matrix falling into the corresponding element.
+     *             For example if permut[5]==3 holds, then the fifth row
+     *             of the LU decomposition in fact is dealing with the third
+     *             row of the original matrix.
+     *
+     * @throws anpi::Exception if matrix cannot be decomposed, or input
+     *         matrix is not square.
+     */
+    template<typename T>
+    void luDoolittle(const Matrix<T> &A,
+                     Matrix<T> &LU,
+                     std::vector<size_t> &permut) {
+        //ddddd
+    }
 
 }
-  
+
 #endif
 
